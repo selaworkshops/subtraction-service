5227a4a0db4cba330935f82736cb09637da368e9
Leon Jalfon <leonj@sela.co.il>
2018-06-11 20:40:02 +0300

    fix response + refactoring

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,24 @@
 var appRouter = function (app) {
-var substraction = require('./../substraction.js');
 
 app.get("/", function(req, res) {
 	res.status(404).send("Error 404, To use the substraction service browse to: {server}:{port}/substraction/{number1}/{number2}");
 })
   
 app.get("/substraction/:number1/:number2", function(req,res) {
-    var number1 = req.params.number1;
-    var number2 = req.params.number2;
-    var total = substraction.substraction(number1,number2);
-    res.send(JSON.stringify(total));
+    var number1 = parseInt(req.params.number1);
+    var number2 = parseInt(req.params.number2);
+    var total = number1 - number2;
+
+	var body = {
+		"first-number": number1,
+		"second-number": number2,
+		"operation": "substraction",
+		"total": total
+	}
+	
+	res.setHeader('Access-Control-Allow-Origin', 'null');
+	return res.status(200).end(JSON.stringify(body));
+	
   });
 }
 
--- substraction.js
+++
@@ -1,6 +0,0 @@
-exports.substraction = function(number1, number2){
-   const num1 = parseInt(number1);
-   const num2 = parseInt(number2);
-   var total = num1 - num2;
-   return total;
-}
