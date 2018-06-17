88c25a41ab1a9dca9e48329b6ce3867bbe22662f
Leon Jalfon <leonj@sela.co.il>
2018-06-18 00:55:40 +0300

    spelling fix

--- routes/routes.js
+++ routes/routes.js
@@ -7,10 +7,10 @@ app.use(function(req, res, next) {
 });
 
 app.get("/", function(req, res) {
-	res.status(404).send("Error 404, To use the substraction service browse to: {server}:{port}/substraction/{number1}/{number2}");
+	res.status(404).send("Error 404, To use the subtraction service browse to: {server}:{port}/substraction/{number1}/{number2}");
 })
   
-app.get("/substraction/:number1/:number2", function(req,res) {
+app.get("/subtraction/:number1/:number2", function(req,res) {
     var number1 = parseInt(req.params.number1);
     var number2 = parseInt(req.params.number2);
     var total = number1 - number2;
@@ -18,7 +18,7 @@ app.get("/substraction/:number1/:number2", function(req,res) {
 	var body = {
 		"first-number": number1,
 		"second-number": number2,
-		"operation": "substraction",
+		"operation": "subtraction",
 		"total": total
 	}
 	
